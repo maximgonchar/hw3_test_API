daedf7e84808e7c2e5735cdc27f2a69bd9f0efa0
Maxim Gonchar <m.gonchar@bi.zone>
2021-05-13 01:50:21 +0300

    create first test set for deg-service without parametrize

--- test_dog_service/test_dog.py
+++ test_dog_service/test_dog.py
@@ -10,21 +10,21 @@ path_random_image = '/breeds/image/random'
 
 
 class TestDogService:
-    def test_list_all_breeds(self, base_url):
-        res = requests.get(base_url + path_list_all_breeds)
+    def test_list_all_breeds_status(self, base_url):
+        res = requests.get(base_url + '/breeds/list/all')
         assert res.status_code == 200
 
-    def test_no_list_all_breeds(self, base_url):
+    def test_not_found_list_all_breeds(self, base_url):
         res = requests.get(base_url + '/' + str(uuid.uuid4()))
         assert res.status_code == 404
 
-    def test_get_random_image(self, base_url):
-        res = requests.get(base_url + path_random_image)
+    def test_get_random_image_status(self, base_url):
+        res = requests.get(base_url + '/breeds/image/random')
         assert res.status_code == 200
 
-    def test_api_json_schema(self, base_url):
+    def test_api_json_schema_one_dog(self, base_url):
         """Проверка структуры ответа на запрос получения рандомного фото собаки"""
-        res = requests.get(base_url + path_random_image)
+        res = requests.get(base_url + '/breeds/image/random').json()
         schema = {
             "type": "object",
             "properties": {
@@ -33,24 +33,29 @@ class TestDogService:
             },
             "required": ["message", "status"]
         }
-        assert validate(instance=res.json(), schema=schema) == None
+        validate(instance=res, schema=schema)
+        assert res['status'] == 'success'
 
-    def test_api_json_schema_cerberus(self, base_url):
+    def test_api_json_schema_cerberus_one_dog(self, base_url):
         """Проверка структуры ответа на запрос получения рандомного фото собаки при помощи cerberus"""
-        res = requests.get(base_url + path_random_image)
+        res = requests.get(base_url + '/breeds/image/random').json()
         schema = {
             "message": {"type": "string"},
             "status": {"type": "string"}
         }
         v = cerberus.Validator()
-        assert v.validate(res.json(), schema)
+        assert v.validate(res, schema)
+        assert res['status'] == 'success'
 
     def test_api_json_schema_cerberus_multiply_breed(self, base_url):
         """Проверка структуры ответа на запрос получения рандомного фото нескольких собак"""
-        res = requests.get(base_url + path_random_image + '/' + str(random.randint(1, 50)))
+        random_dog = random.randint(1, 50)
+        res = requests.get(base_url + '/breeds/image/random/' + str(random_dog)).json()
         schema = {
             "message": {"type": "list"},
             "status": {"type": "string"}
         }
         v = cerberus.Validator()
-        assert v.validate(res.json(), schema)
+        assert v.validate(res, schema)
+        assert res['status'] == 'success'
+        assert len(res['message']) == random_dog
