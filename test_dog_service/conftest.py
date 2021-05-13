752a8a69ff74a8e66766c9c8fcc99988a49f1ca0
Maxim Gonchar <m.gonchar@bi.zone>
2021-05-14 01:46:25 +0300

    refactor tests use fixture

--- test_dog_service/conftest.py
+++ test_dog_service/conftest.py
@@ -1,4 +1,28 @@
+import cerberus
 import pytest
+import requests
+
+url_random_image_from_all_dogs = 'https://dog.ceo/api/breeds/image/random'
+
+
+@pytest.fixture()
+def status_response_random_dog_image():
+    """Возвращает текст из ответа в status"""
+    resp = requests.get(url_random_image_from_all_dogs).json()["status"]
+    return resp
+
+
+@pytest.fixture()
+def status_code_random_dog_image():
+    """Возвращает код ответа выполнении get запроса на корректный урл"""
+    resp = requests.get(url_random_image_from_all_dogs).status_code
+    return resp
+
+
+@pytest.fixture(params=[5, 10, 25, 50])
+def status_response_multiply_random_dog(request):
+    resp = requests.get(url_random_image_from_all_dogs + '/' + f'{request.param}').json()['status']
+    return resp
 
 
 def pytest_addoption(parser):
@@ -13,3 +37,4 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def base_url(request):
     return request.config.getoption("--url")
+
--- test_dog_service/test_dog.py
+++ test_dog_service/test_dog.py
@@ -10,9 +10,7 @@ path_random_image = '/breeds/image/random'
 
 
 class TestDogService:
-    def test_list_all_breeds_status(self, base_url):
-        res = requests.get(base_url + '/breeds/list/all')
-        assert res.status_code == 200
+
 
     def test_not_found_list_all_breeds(self, base_url):
         res = requests.get(base_url + '/' + str(uuid.uuid4()))
@@ -22,7 +20,7 @@ class TestDogService:
         res = requests.get(base_url + '/breeds/image/random')
         assert res.status_code == 200
 
-    def test_api_json_schema_one_dog(self, base_url):
+    def test_api_json_schema_random_dog(self, base_url):
         """Проверка структуры ответа на запрос получения рандомного фото собаки"""
         res = requests.get(base_url + '/breeds/image/random').json()
         schema = {
@@ -36,7 +34,7 @@ class TestDogService:
         validate(instance=res, schema=schema)
         assert res['status'] == 'success'
 
-    def test_api_json_schema_cerberus_one_dog(self, base_url):
+    def test_api_json_schema_cerberus_random_dog(self, base_url):
         """Проверка структуры ответа на запрос получения рандомного фото собаки при помощи cerberus"""
         res = requests.get(base_url + '/breeds/image/random').json()
         schema = {
@@ -47,15 +45,60 @@ class TestDogService:
         assert v.validate(res, schema)
         assert res['status'] == 'success'
 
-    def test_api_json_schema_cerberus_multiply_breed(self, base_url):
-        """Проверка структуры ответа на запрос получения рандомного фото нескольких собак"""
-        random_dog = random.randint(1, 50)
-        res = requests.get(base_url + '/breeds/image/random/' + str(random_dog)).json()
+    # def test_api_json_schema_cerberus_random_dogs(self, base_url):
+    #     """Проверка структуры ответа на запрос получения рандомного фото нескольких собак"""
+    #     random_dog = random.randint(1, 50)
+    #     res = requests.get(base_url + '/breeds/image/random/' + str(random_dog)).json()
+    #     schema = {
+    #         "message": {"type": "list"},
+    #         "status": {"type": "string"}
+    #     }
+    #     v = cerberus.Validator()
+    #     assert v.validate(res, schema)
+    #     assert res['status'] == 'success'
+    #     assert len(res['message']) == random_dog
+
+    @pytest.mark.parametrize("count_dogs", [x for x in range(1, 51)])
+    def test_api_multiply_random_from_all_dogs(self, base_url, count_dogs):
+        """Проверка структуры ответа, статус-кода на запрос получения рандомного фото нескольких собак"""
+        res = requests.get(base_url + f'/breeds/image/random/{count_dogs}')
         schema = {
             "message": {"type": "list"},
             "status": {"type": "string"}
         }
         v = cerberus.Validator()
-        assert v.validate(res, schema)
-        assert res['status'] == 'success'
-        assert len(res['message']) == random_dog
+        assert v.validate(res.json(), schema)
+        assert res.json()['status'] == 'success'
+        assert len(res.json()['message']) == count_dogs
+        assert res.status_code == 200
+
+    @pytest.mark.parametrize("count_dogs", [x for x in range(1, 51)])
+    def test_api_multiply_random_dogs(self, base_url, count_dogs):
+        """Проверка структуры ответа, статус-кода на запрос получения рандомного фото нескольких собак"""
+        res = requests.get(base_url + f'/breed/hound/images/random/{count_dogs}')
+        schema = {
+            "message": {"type": "list"},
+            "status": {"type": "string"}
+        }
+        v = cerberus.Validator()
+        assert v.validate(res.json(), schema)
+        assert res.json()['status'] == 'success'
+        assert len(res.json()['message']) == count_dogs
+        assert res.status_code == 200
+
+    def test_api_all_dog_from_breed_collection(self, base_url):
+        res = requests.get(base_url + '/breed/hound/images')
+        print(len(res.json()['message']))
+
+
+class TestApiDog:
+    def test_status_random_dog(self, status_response_random_dog_image):
+        assert status_response_random_dog_image == 'success'
+
+    def test_status_code_random_dog(self, status_code_random_dog_image):
+        assert status_code_random_dog_image == 200
+
+    def test_status_multiply_random_dog(self, status_response_multiply_random_dog):
+        assert status_response_multiply_random_dog == 'success'
+
+
